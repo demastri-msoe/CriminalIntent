fa7aa48ee93e2cab72a1def2673d88a45aca7c53
John DeMastri <demastri@msoe.edu>
2023-07-27 17:41:57 -0500

    Chapter 10 Challenges - Interesting challenge - add a second type of view based on parameter of crime object - needed to build a view hierarchy and stitch it into the framework

--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeListAdapter.kt
@@ -6,14 +6,13 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.msoe.bnrtextapps.criminalintent.databinding.ListItemCrimeBinding
+import com.msoe.bnrtextapps.criminalintent.databinding.ListItemCrimePoliceRequiredBinding
 
-class CrimeHolder(
-    val binding: ListItemCrimeBinding
+open class CrimeHolderBase (
+    internal val binding: ViewBinding
+    // once this class maintains the bindings (see below), this can be private
 ) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(crime: Crime) {
-        binding.crimeTitle.text = crime.title
-        binding.crimeDate.text = crime.date.toString()
-
+    open fun bind(crime: Crime) {
         binding.root.setOnClickListener {
             Toast.makeText(
                 binding.root.context,
@@ -24,23 +23,58 @@ class CrimeHolder(
     }
 }
 
+class CrimeHolder(
+    val thisbinding:ViewBinding
+    // once this class maintains the bindings (see below), this can be private
+) : CrimeHolderBase(thisbinding) {
+    override fun bind(crime: Crime) {
+        super.bind(crime)
+        var baseBinding: ListItemCrimeBinding = binding as ListItemCrimeBinding
+        baseBinding.crimeTitle.text = crime.title
+        baseBinding.crimeDate.text = crime.date.toString()
+    }
+}
+
+class CrimeHolderPolice(
+    val thisbinding: ViewBinding
+    // once this class maintains the bindings (see below), this can be private
+) : CrimeHolderBase(thisbinding) {
+    override fun bind(crime: Crime) {
+        super.bind(crime)
+        var baseBinding: ListItemCrimePoliceRequiredBinding = binding as ListItemCrimePoliceRequiredBinding
+        baseBinding.crimeTitle.text = crime.title
+    }
+}
+
 class CrimeListAdapter(
     private val crimes: List<Crime>
-) : RecyclerView.Adapter<CrimeHolder>() {
+) : RecyclerView.Adapter<CrimeHolderBase>() {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ) : CrimeHolder {
+    ) : CrimeHolderBase {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
-        return CrimeHolder(binding)
+        if(viewType == 0) {
+            val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+            return CrimeHolder(binding)
+        } else {
+            val binding = ListItemCrimePoliceRequiredBinding.inflate(inflater, parent, false)
+            return CrimeHolderPolice(binding)
+        }
     }
 
-    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+    override fun onBindViewHolder(holder: CrimeHolderBase, position: Int) {
         val crime = crimes[position]
         holder.bind(crime)
     }
 
     override fun getItemCount() = crimes.size
+
+    override fun getItemViewType(position: Int): Int {
+        if(crimes[position].requiresPolice)
+            return 1
+        return 0
+    }
+
 }
