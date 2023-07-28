dc4e3d322ab6f8cd45cc94664ba83d55b2ab7064
John DeMastri <demastri@msoe.edu>
2023-07-28 15:16:16 -0500

    Chapter 12 - p145 after listing 12.7 - moved job management to repeatOnLifecycle() and cleaned up some code

--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeListFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.msoe.bnrtextapps.criminalintent.databinding.FragmentCrimeListBinding
 import kotlinx.coroutines.Job
@@ -18,7 +20,6 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
 
     private var _binding: FragmentCrimeListBinding? = null
-    private var job: Job? = null
 
     private val binding
         get() = checkNotNull(_binding) {
@@ -26,11 +27,6 @@ class CrimeListFragment : Fragment() {
         }
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,24 +36,19 @@ class CrimeListFragment : Fragment() {
         _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        val crimes = crimeListViewModel.crimes
-        val adapter = CrimeListAdapter(crimes)
-        binding.crimeRecyclerView.adapter = adapter
-
         return binding.root
     }
-    override fun onStart() {
-        super.onStart()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        job = viewLifecycleOwner.lifecycleScope.launch {
-            val crimes = crimeListViewModel.loadCrimes()
-            binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val crimes = crimeListViewModel.loadCrimes()
+                Log.d(TAG, "Total crimes: ${crimes.size}")
+                binding.crimeRecyclerView.adapter =
+                    CrimeListAdapter(crimes)
+            }
         }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        job?.cancel()
     }
     override fun onDestroyView() {
         super.onDestroyView()
