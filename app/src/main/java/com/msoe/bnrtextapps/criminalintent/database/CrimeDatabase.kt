bef8e43b582ae665814e1b76a31f3e332fedaf85
John DeMastri <demastri@msoe.edu>
2023-07-31 11:36:16 -0500

    Chapter 12 Challenges - remove the compiler processor warning: either provide a location for the schema export to write to (in build.gradle) or disable the export entirely (in CrimeDatabase.kt)

--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/database/CrimeDatabase.kt
@@ -6,7 +6,8 @@ import androidx.room.TypeConverters
 import com.msoe.bnrtextapps.criminalintent.Crime
 
 
-@Database(entities = [ Crime::class ], version=1)
+// See app build.gradle - exportSchema=false solves the challenge in Ch 12 by disabling the export
+@Database(entities = [ Crime::class ], version=1, exportSchema=false)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
     abstract fun crimeDao(): CrimeDao
