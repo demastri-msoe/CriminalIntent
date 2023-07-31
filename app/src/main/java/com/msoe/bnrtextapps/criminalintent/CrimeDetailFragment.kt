ea7fd3b59c65089a316a7897a548b194b648d8f7
John DeMastri <demastri@msoe.edu>
2023-07-31 13:44:15 -0500

    Revert "Chapter 13 - p165 after listing 13.19 - details from object now displayed on detail view fragment"
    This reverts commit 27e6ecd64f87f88d6662ee12a42ee4d3c2d14aa6.

--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeDetailFragment.kt
@@ -7,13 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
 import com.msoe.bnrtextapps.criminalintent.databinding.FragmentCrimeDetailBinding
-import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
@@ -25,10 +20,19 @@ class CrimeDetailFragment : Fragment() {
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+    private lateinit var crime: Crime
     private val args: CrimeDetailFragmentArgs by navArgs()
 
-    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
-        CrimeDetailViewModelFactory(args.crimeId)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+        Log.d(TAG, "The crime ID is: ${args.crimeId}")
     }
 
     override fun onCreateView(
@@ -46,33 +50,19 @@ class CrimeDetailFragment : Fragment() {
 
         binding.apply {
             crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
             }
             crimeDate.apply {
+                text = crime.date.toString()
                 isEnabled = false
             }
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-            }
-        }
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                crimeDetailViewModel.crime.collect { crime ->
-                    crime?.let { updateUi(it) }
-                }
+                crime = crime.copy(isSolved = isChecked)
             }
         }
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun updateUi(crime: Crime) {
-        binding.apply {
-            if (crimeTitle.text.toString() != crime.title) {
-                crimeTitle.setText(crime.title)
-            }
-            crimeDate.text = crime.date.toString()
-            crimeSolved.isChecked = crime.isSolved
-        }
     }
 }
--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeListViewModel.kt
@@ -1,11 +1,18 @@
 package com.msoe.bnrtextapps.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.util.Date
+import java.util.UUID
+import kotlin.coroutines.CoroutineContext
 
 private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
