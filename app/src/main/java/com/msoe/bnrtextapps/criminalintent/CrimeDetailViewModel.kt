06ed2c983d05bd798228b46be1dc1b12d6d61858
John DeMastri <demastri@msoe.edu>
2023-08-15 11:33:12 -0500

    Chapter 13 - p167 after listing 13.26 - updating crime detail - now data is being saved to db

--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeDetailViewModel.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeDetailViewModel.kt
@@ -37,9 +37,7 @@ class CrimeDetailViewModel(crimeId: UUID) : ViewModel() {
     override fun onCleared() {
         super.onCleared()
 
-        viewModelScope.launch {
-            crime.value?.let { crimeRepository.updateCrime(it) }
-        }
+        crime.value?.let { crimeRepository.updateCrime(it) }
     }
 }
 
