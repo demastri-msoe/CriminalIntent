8f2c8126f8cb7bbcd7fe31d4f2e6ad7c75f45621
John DeMastri <demastri@msoe.edu>
2023-08-15 12:24:43 -0500

    Chapter 13 Challenges - added check to prevent write of empty title to database by overriding back functionality

--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeDetailFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.msoe.bnrtextapps.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
+
 
 private const val TAG = "CrimeDetailFragment"
 class CrimeDetailFragment : Fragment() {
@@ -35,6 +39,26 @@ class CrimeDetailFragment : Fragment() {
     ): View? {
         _binding =
             FragmentCrimeDetailBinding.inflate(inflater, container, false)
+
+        // This callback is only called when MyFragment is at least started
+        requireActivity().onBackPressedDispatcher.addCallback(this,
+            object:OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                binding.apply {
+                    if (crimeTitle.text.toString().trim() == "") {
+                        val snackbar = getView()?.let {
+                            Snackbar
+                                .make(it, "Please supply a title for the crime...", Snackbar.LENGTH_LONG)
+                        }
+                        snackbar?.show()
+                    } else {
+                        findNavController().popBackStack()
+                    }
+                }
+            }
+            }
+        )
+
         return binding.root
     }
 
