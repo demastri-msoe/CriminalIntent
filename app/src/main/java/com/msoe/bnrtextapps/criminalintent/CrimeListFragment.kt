85465efe6db808ac688ade79441aaad817c5590b
John DeMastri <demastri@msoe.edu>
2023-09-29 10:45:00 -0500

    Chapter 15 - p179 after listing 15.4 - updated theme, added menu asset (changed tint) and menu to app bar

--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/CrimeListFragment.kt
@@ -3,6 +3,8 @@ package com.msoe.bnrtextapps.criminalintent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -30,6 +32,11 @@ class CrimeListFragment : Fragment() {
         }
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,4 +71,10 @@ class CrimeListFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
 }
