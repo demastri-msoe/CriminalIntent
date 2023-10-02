038ba9ed3ccb261c1fe18ea753b61ae28aada508
John DeMastri <demastri@msoe.edu>
2023-10-01 20:29:32 -0500

    Chapter 16 - p 188 after listing 16.5 - migrate database to v2

--- app/src/main/java/com/msoe/bnrtextapps/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/msoe/bnrtextapps/criminalintent/database/CrimeDatabase.kt
@@ -3,11 +3,21 @@ package com.msoe.bnrtextapps.criminalintent.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.msoe.bnrtextapps.criminalintent.Crime
 
 
-@Database(entities = [ Crime::class ], version=1)
+@Database(entities = [ Crime::class ], version=2)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
     abstract fun crimeDao(): CrimeDao
+}
+
+val migration_1_2 = object : Migration(1,2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "Alter Table Crime ADD Column suspect TEXT NOT NULL DEFAULT ''"
+        )
+    }
 }
